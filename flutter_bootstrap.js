3751b12d3aa1b331b6b12031d7dfa0a538d66acb
kcierpisz <26062599+Kamil3001@users.noreply.github.com>
2024-12-11 23:32:46 +0000

    adding helper for trap symbols

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2114791687"
+    serviceWorkerVersion: "1262713291"
   }
 });
