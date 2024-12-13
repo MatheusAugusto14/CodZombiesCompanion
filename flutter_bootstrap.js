33e16906f57d05ad1fa365171a7521b58207ced9
kcierpisz <26062599+Kamil3001@users.noreply.github.com>
2024-12-13 01:51:19 +0000

    Added some additional features to the helpers, such as buttons to enlarge or change images

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2178442533"
+    serviceWorkerVersion: "4147132462"
   }
 });
