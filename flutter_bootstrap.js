1cf73373050f704f1ebaade1e6e1c597f49ddb5c
kcierpisz <26062599+Kamil3001@users.noreply.github.com>
2024-12-28 01:24:03 +0000

    fixing icons hopefully

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "661608609"
+    serviceWorkerVersion: "3173139706"
   }
 });
